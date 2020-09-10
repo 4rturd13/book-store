46df2df4a8170db1c968ccf298649a21df7f7047
4rturd13 <4rturd13@gmail.com>
2020-09-09 21:17:43 -0500

    upload project & configuration database orm

---
+++ src/database/migrations/1599677033910-first_migration.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class firstMigration1599677033910 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
