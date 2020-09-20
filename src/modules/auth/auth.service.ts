8a8d0d4a4e038c69771c6dd43001a80e2fab43ca
4rturd13 <4rturd13@gmail.com>
2020-09-19 22:19:28 -0500

    add: auth module & user signin - signup done

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,64 @@
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AuthRepository } from './auth.repository';
+import { SignupDto } from './dto/signup.dto';
+import { SigninDto } from './dto/signin.dto';
+import { User } from '../user/user.entity';
+import { compare } from 'bcryptjs';
+import { IJwtPayload } from './jwt-payload.interface';
+import { RoleType } from '../role/roletype.enum';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(AuthRepository)
+    private readonly _authRepository: AuthRepository,
+    private readonly _jwtService: JwtService,
+  ) {}
+
+  async signup(SignupDto: SignupDto): Promise<void> {
+    const { username, email } = SignupDto;
+    const userExists = await this._authRepository.findOne({
+      where: [{ username }, { email }],
+    });
+
+    if (userExists) {
+      throw new ConflictException('Username or Email already exists');
+    }
+    return this._authRepository.signup(SignupDto);
+  }
+
+  async signin(SigninDto: SigninDto): Promise<{ token: string }> {
+    const { username, password } = SigninDto;
+
+    const user: User = await this._authRepository.findOne({
+      where: { username },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User does not exist');
+    }
+
+    const isMatch = await compare(password, user.password);
+
+    if (!isMatch) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const payload: IJwtPayload = {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+      roles: user.roles.map(r => r.name as RoleType),
+    };
+    const token = await this._jwtService.sign(payload);
+
+    return { token };
+  }
+}
